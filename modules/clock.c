de419ea32143c9c7040959fc4c75cae34f3ad700
Angelo Arrifano <miknix@gmail.com>
2012-02-18 00:08:48 +0000

    clock: Code cleanup
    Remove dead code, format files according to kernel.org guidelines

--- modules/clock.c
+++ modules/clock.c
@@ -1,62 +1,43 @@
-// *************************************************************************************************
-//
-//	Copyright (C) 2009 Texas Instruments Incorporated - http://www.ti.com/
-//
-//
-//	  Redistribution and use in source and binary forms, with or without
-//	  modification, are permitted provided that the following conditions
-//	  are met:
-//
-//	    Redistributions of source code must retain the above copyright
-//	    notice, this list of conditions and the following disclaimer.
-//
-//	    Redistributions in binary form must reproduce the above copyright
-//	    notice, this list of conditions and the following disclaimer in the
-//	    documentation and/or other materials provided with the
-//	    distribution.
-//
-//	    Neither the name of Texas Instruments Incorporated nor the names of
-//	    its contributors may be used to endorse or promote products derived
-//	    from this software without specific prior written permission.
-//
-//	  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//	  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-//	  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-//	  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-//	  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-//	  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//	  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-//	  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-//	  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-//	  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-//	  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// *************************************************************************************************
+/******************************************************************************
+    Copyright (C) 2009 Texas Instruments Incorporated - http://www.ti.com/
 
-// system
-#include <ezchronos.h>
 
-// driver
-#include "ports.h"
-#include "display.h"
-#include "timer.h"
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
 
-// logic
+      Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+      Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the
+      distribution.
+
+      Neither the name of Texas Instruments Incorporated nor the names of
+      its contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
 #include "clock.h"
 
-#ifdef CONFIG_SIDEREAL
-#include "sidereal.h"
-#endif
+/* driver */
+#include "ports.h"
+#include "display.h"
+#include "rtca.h"
 
-#include "date.h"
-
-#ifdef CONFIG_CLOCK_ONLY_SYNC
-#include "rfsimpliciti.h"
-#endif
-
-
-// *************************************************************************************************
-// Global Variable section
 struct time sTime;
 
 void clock_event(rtca_tevent_ev_t ev)
@@ -105,14 +86,17 @@ static void edit_inc()
 	switch (sTime.edit_state) {
 	case EDIT_STATE_MM:
 		helpers_loop_up(&sTime.tmp_min , 0, 60);
+
 		display_chars(LCD_SEG_L1_1_0, _itoa(sTime.tmp_min, 2, 0),
-																				SEG_ON_BLINK_ON);
+							SEG_ON_BLINK_ON);
 		break;
 
 	case EDIT_STATE_HH:
-		helpers_loop_up(&sTime.tmp_hour, 0, 24);  /* TODO: fix for 12/24 hr! */
+		/* TODO: fix for 12/24 hr! */
+		helpers_loop_up(&sTime.tmp_hour, 0, 24);
+
 		display_chars(LCD_SEG_L1_3_2, _itoa(sTime.tmp_hour, 2, 0),
-																				SEG_ON_BLINK_ON);
+							SEG_ON_BLINK_ON);
 		break;
 	default:
 		break;
@@ -124,14 +108,17 @@ static void edit_dec()
 	switch (sTime.edit_state) {
 	case EDIT_STATE_MM:
 		helpers_loop_down(&sTime.tmp_min , 0, 60);
+
 		display_chars(LCD_SEG_L1_1_0, _itoa(sTime.tmp_min, 2, 0),
-																				SEG_ON_BLINK_ON);
+							SEG_ON_BLINK_ON);
 		break;
 
 	case EDIT_STATE_HH:
-		helpers_loop_down(&sTime.tmp_hour, 0, 24);  /* TODO: fix for 12/24 hr! */
+		/* TODO: fix for 12/24 hr! */
+		helpers_loop_down(&sTime.tmp_hour, 0, 24);
+
 		display_chars(LCD_SEG_L1_3_2, _itoa(sTime.tmp_hour, 2, 0),
-																				SEG_ON_BLINK_ON);
+							SEG_ON_BLINK_ON);
 		break;
 	default:
 		break;
@@ -143,9 +130,11 @@ static void edit_next()
 	helpers_loop_up(&sTime.edit_state, EDIT_STATE_HH, EDIT_STATE_MM);
 
 	display_chars(LCD_SEG_L1_1_0, _itoa(sTime.tmp_min, 2, 0),
-		(sTime.edit_state==EDIT_STATE_MM? SEG_ON_BLINK_ON : SEG_ON_BLINK_OFF));
+		(sTime.edit_state == EDIT_STATE_MM ?
+					SEG_ON_BLINK_ON : SEG_ON_BLINK_OFF));
 	display_chars(LCD_SEG_L1_3_2, _itoa(sTime.tmp_hour, 2, 0),
-		(sTime.edit_state==EDIT_STATE_HH? SEG_ON_BLINK_ON : SEG_ON_BLINK_OFF));
+		(sTime.edit_state == EDIT_STATE_HH ?
+					SEG_ON_BLINK_ON : SEG_ON_BLINK_OFF));
 }
 
 static void edit_save()
--- modules/clock.h
+++ modules/clock.h
@@ -1,77 +1,59 @@
-// *************************************************************************************************
-//
-//	Copyright (C) 2009 Texas Instruments Incorporated - http://www.ti.com/
-//
-//
-//	  Redistribution and use in source and binary forms, with or without
-//	  modification, are permitted provided that the following conditions
-//	  are met:
-//
-//	    Redistributions of source code must retain the above copyright
-//	    notice, this list of conditions and the following disclaimer.
-//
-//	    Redistributions in binary form must reproduce the above copyright
-//	    notice, this list of conditions and the following disclaimer in the
-//	    documentation and/or other materials provided with the
-//	    distribution.
-//
-//	    Neither the name of Texas Instruments Incorporated nor the names of
-//	    its contributors may be used to endorse or promote products derived
-//	    from this software without specific prior written permission.
-//
-//	  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//	  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-//	  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-//	  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-//	  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-//	  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//	  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-//	  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-//	  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-//	  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-//	  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// *************************************************************************************************
+/******************************************************************************
+    Copyright (C) 2009 Texas Instruments Incorporated - http://www.ti.com/
 
-#ifndef CLOCKTIMER_H_
-#define CLOCKTIMER_H_
 
-#include "rtca.h"
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
 
-// *************************************************************************************************
-// Defines section
+      Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
 
-// Definitions for time format
+      Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the
+      distribution.
+
+      Neither the name of Texas Instruments Incorporated nor the names of
+      its contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#ifndef __CLOCK_H__
+#define __CLOCK_H__
+
+#include "ezchronos.h"
+
+/* Definitions for time format */
 #define TIMEFORMAT_24H					(0u)
 #define TIMEFORMAT_12H					(1u)
 
-/* TODO: display_time should be private scope! */
-extern void clock_event(rtca_tevent_ev_t ev);
-
-
-
-// *************************************************************************************************
-// Global Variable section
-/* TODO: pack this stuff!!!! */
-struct time
-{
+struct time {
 	enum {
-       EDIT_STATE_OFF		= 0,
-	    EDIT_STATE_HH			= 1,
-	    EDIT_STATE_MM			= 2,
+		EDIT_STATE_OFF	= 0,
+		EDIT_STATE_HH	= 1,
+		EDIT_STATE_MM	= 2,
 	} edit_state;
 
-	/* Temporary variables for editing */
+	/* Temporary variables for edit mode */
 	uint8_t tmp_hour;
 	uint8_t tmp_min;
 	uint8_t tmp_sec;
-
-#ifdef CONFIG_SIDEREAL
-	// offset of local time from UTC (=1: set time is UTC+1 =CET)
-	int8_t		UTCoffset;
-#endif
 };
+
+/* TODO: make this struct private */
 extern struct time sTime;
 
-
-#endif /*CLOCKTIMER_H_*/
+#endif /* __CLOCK_H__ */
