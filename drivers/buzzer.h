7015b7aeecef0dfc718bd42761bba60d74d020ef
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2012-03-27 21:06:41 +0200

    Converted buzzer state to enum
    Converted to enum so value consistency is enforced. Shouldn't be more
    memory-hungry than using defines. I also moved some code around, just
    for aestethics. I removed unused defines for buzzer mode, too.

--- drivers/buzzer.h
+++ drivers/buzzer.h
@@ -43,26 +43,23 @@
 
 // *************************************************************************************************
 // Prototypes section
-extern void reset_buzzer(void);
-extern void start_buzzer(uint8_t cycles, uint16_t on_time, uint16_t off_time);
-extern void start_buzzer_steps(uint8_t cycles, uint16_t on_time, uint16_t off_time, uint8_t steps);
-extern void stop_buzzer(void);
-extern void toggle_buzzer(void);
-extern uint8_t is_buzzer(void);
+extern void	reset_buzzer(void);
+extern void	start_buzzer(uint8_t cycles, uint16_t on_time, uint16_t off_time);
+extern void	start_buzzer_steps(uint8_t cycles, uint16_t on_time, uint16_t off_time, uint8_t steps);
+extern void	stop_buzzer(void);
+extern void	toggle_buzzer(void);
+extern uint8_t	is_buzzer(void);
 
+// Buzzer states
+typedef enum {
+    BUZZER_OFF,
+    BUZZER_ON_OUTPUT_DISABLED,
+    BUZZER_ON_OUTPUT_ENABLED
+} buzzer_state;
 
 // *************************************************************************************************
 // Defines section
 
-// Buzzer states
-#define BUZZER_OFF				(0u)
-#define BUZZER_ON_OUTPUT_DISABLED		(1u)
-#define BUZZER_ON_OUTPUT_ENABLED		(2u)
-
-// Buzzer modes
-#define BUZZER_MODE_SINGLE			(0u)
-#define BUZZER_MODE_SINGLE_CONTINUOUS		(1u)
-#define BUZZER_MODE_DOUBLE_CONTINUOUS		(2u)
 
 // Buzzer output signal frequency = 32,768kHz/(BUZZER_TIMER_STEPS+1)/2 = 2.7kHz
 #define BUZZER_TIMER_STEPS			(5u)
@@ -80,24 +77,21 @@ struct buzzer {
 	// Keep output for "time" seconds
 	uint8_t time;
 
-	// On/off duty
+	// On/off duty in ticks
 	uint16_t on_time;
 	uint16_t off_time;
 
 	// Current buzzer output state
-	uint8_t state;
+	buzzer_state state;
 
 	// Current steps (~freq)
+	// Frequency = f_ACLK/((steps+1) * 2)
 	uint8_t steps;
 
 };
-extern struct buzzer sBuzzer;
-
 
 // *************************************************************************************************
 // Extern section
-
-
-
+extern struct buzzer sBuzzer;
 
 #endif /*BUZZER_H_*/
