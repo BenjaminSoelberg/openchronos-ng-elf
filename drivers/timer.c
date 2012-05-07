7ee84c6065d6e014812251a77c8b609062564ad5
Angelo Arrifano <miknix@gmail.com>
2012-05-08 00:28:55 +0200

    drivers/timer: Drop idea on several programable timers
    It was decided that we need:
     TA0CCR0: 100ms timer (TODO)
     TA0CCR1: Unused
     TA0CCR2: Unused
     TA0CCR3: programable timer (TODO)
     TA0CCR4: delay timer
    OVERFLOW: 1Hz timer (TODO)
    
    So, this patch cleanups the driver making it ready for the new
    functionality.
    I also fixed a bug on timer0_init where CCIE was being used to enable
    the overflow interrupt instead of TAIE.

--- drivers/timer.c
+++ drivers/timer.c
@@ -16,76 +16,80 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/*	  Copyright (C) 2009 Texas Instruments Incorporated - http://www.ti.com/
+
+
+	  Redistribution and use in source and binary forms, with or without
+	  modification, are permitted provided that the following conditions
+	  are met:
+
+	    Redistributions of source code must retain the above copyright
+	    notice, this list of conditions and the following disclaimer.
+
+	    Redistributions in binary form must reproduce the above copyright
+	    notice, this list of conditions and the following disclaimer in the
+	    documentation and/or other materials provided with the
+	    distribution.
+
+	    Neither the name of Texas Instruments Incorporated nor the names of
+	    its contributors may be used to endorse or promote products derived
+	    from this software without specific prior written permission.
+
+	  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+	  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+	  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+	  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+	  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+	  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+	  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include "timer.h"
+
+/* HARDWARE TIMER ASSIGNMENT:
+	 TA0CCR0: 100ms timer (TODO)
+	 TA0CCR1: Unused
+	 TA0CCR2: Unused
+	 TA0CCR3: programable timer (TODO)
+	 TA0CCR4: delay timer
+	OVERFLOW: 1Hz timer (TODO) */
 
 /* source is 32kHz with /2 divider */
 #define TIMER0_FREQ 16000
 
-/* list of interrupt sources */
-enum timer0_IS {
-	TIMER0_IS_CCR0 = 0,
-	TIMER0_IS_CCR1 = TA0IV_TA0CCR1,
-	TIMER0_IS_CCR2 = TA0IV_TA0CCR2,
-	TIMER0_IS_CCR3 = TA0IV_TA0CCR3,
-	TIMER0_IS_CCR4 = TA0IV_TA0CCR4,
-	TIMER0_IS_OVERFLOW = TA0IV_TA0IFG
-};
-
-
-static struct {
-	uint8_t inuse:1;
-	uint8_t mode:1; /* 0: timer, 1: delay */
-	volatile uint8_t intr:1;
-	uint16_t ticks;
-	void (*callback_fn)(void);
-} timer0_timers[5];
-
+static volatile uint8_t delay_finished;
 
 /* this function setups a 1Hz timer ticked every overflow interrupt */
 void timer0_init(void)
 {
-	/* disable overflow interrupts */
-	TA0CCTL0 &= ~CCIE;
+	/* Enable overflow interrupts */
+	TA0CTL |= TAIE;
 
 	/* select external 32kHz source, /2 divider, continous mode */
 	TA0CTL |= TASSEL__ACLK | ID__2 | MC__CONTINOUS;
-
-	/* init timers vector */
-	uint8_t i = 0;
-	while (i < sizeof(timer0_timers))
-		timer0_timers[i++].inuse = 0;
-}
-
-static int8_t timer0_find_free_timer(void)
-{
-	int8_t i = -1;
-	while (++i < sizeof(timer0_timers)) {
-		if (!timer0_timers[i].inuse)
-			return i;
-	}
-	return -1;
 }
 
 /* This function was based on original Texas Instruments implementation,
    see LICENSE-TI for more information. */
-int8_t timer0_delay(uint16_t duration)
+void timer0_delay(uint16_t duration)
 {
-	int8_t tid = timer0_create_timer(duration, NULL);
+	uint16_t ticks = (TIMER0_FREQ / 1000) * duration;
 	
-	/* no cookie for you */
-	if (tid < 0)
-		return -1;
+	delay_finished = 0;
 
-	timer0_timers[tid].mode = 1;
-	timer0_timers[tid].intr = 0;
+	/* Set next CCR match */
+	TA0CCR4 = TA0R + ticks;
 
-	timer0_start_timer(tid);
+	/* enable interrupt */
+	TA0CCTL4 |= CCIE;
 
 	/* Wait for interrupt */
 	while (1) {
 		/* enter low power mode */
-		// Go to LPM3
 		_BIS_SR(LPM3_bits + GIE);
 		__no_operation();
 
@@ -94,104 +98,41 @@ int8_t timer0_delay(uint16_t duration)
 		WDTCTL = WDTPW + WDTIS__512K + WDTSSEL__ACLK + WDTCNTCL;
 #endif
 
-		/* The interrupt routine sets ticks to zero to signal us
+		/* The interrupt routine sets delay_finished to signal us
 		   that a interrupt happened */
-		if (timer0_timers[tid].intr == 1)
+		if (delay_finished == 1)
 			break;
 	}
 
-	timer0_destroy_timer(tid);
-
-	return tid;
-}
-
-/* create a timer:
- * duration is in miliseconds, min=1, max=1000 */
-int8_t timer0_create_timer(uint16_t duration, void (*callback_fn)(void))
-{
-	__disable_interrupt();
-	int8_t tid = timer0_find_free_timer();
-
-	/* no cookie for you */
-	if (tid < 0)
-		return -1;
-
-	timer0_timers[tid].inuse = 1;
-	__enable_interrupt();
-
-	timer0_timers[tid].mode = 0;
-	timer0_timers[tid].ticks = (TIMER0_FREQ / 1000) * duration;
-	timer0_timers[tid].callback_fn = callback_fn;
-
-	return tid;
-}
-
-void timer0_destroy_timer(int8_t tid)
-{
-	/* stop timer, just in case */
-	timer0_stop_timer(tid);
-
-	/* free up struct */
-	timer0_timers[tid].inuse = 0;
-}
-
-void timer0_start_timer(int8_t tid)
-{
-	uint16_t *ta0ccrn = (uint16_t *)TA0CCR0 + tid*2;
-	uint16_t *ta0cctln = (uint16_t *)TA0CCTL0 + tid*2;
-
-	/* Set next CCR match */
-	*ta0ccrn = TA0R + timer0_timers[tid].ticks;
-
-	/* enable interrupt */
-	*ta0cctln |= CCIE;
-}
-
-void timer0_stop_timer(int8_t tid)
-{
-	uint16_t *ta0cctln = (uint16_t *)TA0CCTL0 + tid*2;
-
 	/* disable interrupt */
-	*ta0cctln &= ~CCIE;
+	TA0CCTL4 |= CCIE;
 }
 
-/* unified interrupt vector A0 + A1 */
-static void timer0_ISR(enum timer0_IS source)
-{
-	if (source == TIMER0_IS_OVERFLOW) {
-		/* 1Hz timer, nothing to do yet */
-		return;
-	}
-	
-	int8_t tid = source >> 1;
-
-	uint16_t *ta0ccrn = (uint16_t *)TA0CCR0 + tid*2;
-	uint16_t *ta0cctln = (uint16_t *)TA0CCTL0 + tid*2;
-
-	/* reset flag */
-	*ta0cctln &= ~CCIFG;
-
-	if (timer0_timers[tid].mode == 0) {
-		/* update CCR for next match */
-		*ta0ccrn += timer0_timers[tid].ticks;
-
-		timer0_timers[tid].callback_fn();
-	} else
-		timer0_timers[tid].intr = 1;
-}
 
 /* interrupt vector for CCR0 */
 __attribute__((interrupt(TIMER0_A0_VECTOR)))
 void timer0_A0_ISR(void)
 {
-	timer0_ISR(TIMER0_IS_CCR0);
+	
 }
 
 /* interrupt vector for CCR1-4 and overflow */
 __attribute__((interrupt(TIMER0_A1_VECTOR)))
 void timer0_A1_ISR(void)
 {
-	timer0_ISR(TA0IV);
+	/* reading TA0IV automatically resets the interrupt flag */
+	uint8_t flag = TA0IV;
+
+	/* 1Hz timer, nothing to do yet */
+	if (flag == TA0IV_TA0IFG) {
+		return;
+	}
+	
+	/* delay timer */
+	if (flag == TA0IV_TA0CCR4) {
+		delay_finished = 1;
+		return;
+	}
 }
 
 
