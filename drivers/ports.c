fe42150f31b6b53ed483998cdb537a59a9f67221
Angelo Arrifano <miknix@gmail.com>
2012-05-10 21:43:15 +0200

    drivers/ports: Move #defines to private scope
    Also deleted unused ones.

--- drivers/ports.c
+++ drivers/ports.c
@@ -42,6 +42,60 @@
 #include "display.h"
 
 
+/* Port, pins and interrupt resources for buttons */
+#define BUTTONS_IN				(P2IN)
+#define BUTTONS_OUT				(P2OUT)
+#define BUTTONS_DIR				(P2DIR)
+#define BUTTONS_REN				(P2REN)
+#define BUTTONS_IE				(P2IE)
+#define BUTTONS_IES				(P2IES)
+#define BUTTONS_IFG				(P2IFG)
+#define BUTTONS_IRQ_VECT2		(PORT2_VECTOR)
+
+/* Button ports */
+#define BUTTON_STAR_PIN			(BIT2)
+#define BUTTON_NUM_PIN			(BIT1)
+#define BUTTON_UP_PIN			(BIT4)
+#define BUTTON_DOWN_PIN			(BIT0)
+#define BUTTON_BACKLIGHT_PIN	(BIT3)
+#define ALL_BUTTONS \
+	(BUTTON_STAR_PIN + BUTTON_NUM_PIN + BUTTON_UP_PIN \
+	  + BUTTON_DOWN_PIN + BUTTON_BACKLIGHT_PIN)
+
+/* Macros for button press detection */
+#define BUTTON_STAR_IS_PRESSED \
+	((BUTTONS_IN & BUTTON_STAR_PIN) == BUTTON_STAR_PIN)
+
+#define BUTTON_NUM_IS_PRESSED	\
+	((BUTTONS_IN & BUTTON_NUM_PIN) == BUTTON_NUM_PIN)
+
+#define BUTTON_UP_IS_PRESSED \
+	((BUTTONS_IN & BUTTON_UP_PIN) == BUTTON_UP_PIN)
+
+#define BUTTON_DOWN_IS_PRESSED \
+	((BUTTONS_IN & BUTTON_DOWN_PIN) == BUTTON_DOWN_PIN)
+
+#define BUTTON_BACKLIGHT_IS_PRESSED \
+	((BUTTONS_IN & BUTTON_BACKLIGHT_PIN) == BUTTON_BACKLIGHT_PIN)
+
+#define NO_BUTTON_IS_PRESSED \
+	((BUTTONS_IN & ALL_BUTTONS) == 0)
+
+/* Macros for button release detection */
+#define BUTTON_STAR_IS_RELEASED			((BUTTONS_IN & BUTTON_STAR_PIN) == 0)
+#define BUTTON_NUM_IS_RELEASED			((BUTTONS_IN & BUTTON_NUM_PIN) == 0)
+#define BUTTON_UP_IS_RELEASED				((BUTTONS_IN & BUTTON_UP_PIN) == 0)
+#define BUTTON_DOWN_IS_RELEASED			((BUTTONS_IN & BUTTON_DOWN_PIN) == 0)
+#define BUTTON_BACKLIGHT_IS_RELEASED	((BUTTONS_IN & BUTTON_BACKLIGHT_PIN) == 0)
+
+/* Button debounce time (msec) */
+#define BUTTONS_DEBOUNCE_TIME_IN	(5u)
+#define BUTTONS_DEBOUNCE_TIME_OUT	(250u)
+#define BUTTONS_DEBOUNCE_TIME_LEFT	(50u)
+
+/* Detect if STAR / NUM button is held low continuously */
+#define LEFT_BUTTON_LONG_TIME		(1u)
+
 /* Macro for button IRQ */
 #define IRQ_TRIGGERED(flags, bit)		((flags & bit) == bit)
 
