BenjaminSoelberg/openchronos-ng-elf

commit f9d41e2aa197964334248c16ffb7538529afd262

Add config options to disable interrupts
This is useful for debugging. Also, in the future we can use this
to enable/disable interrupts according to which modules are enabled.
If no module needs a 10Hz timer, then the 10Hz interrupt can be disabled.

tools/config.py
@@ -75,6 +75,24 @@ DATA["USE_WATCHDOG"] = {
 	"help": "Protects the clock against deadlocks by rebooting it.",
 }
 
+DATA["CONFIG_RTCA_IRQ"] = {
+	"name": "Enable RTC interrupts",
+	"default": True,
+	"help": "Enables interrupts on the Real Time Clock",
+}
+
+DATA["CONFIG_TIMER_1HZ_IRQ"] = {
+	"name": "Enable 1Hz timer interrupts",
+	"default": True,
+	"help": "Enables 1Hz interrupts on the hardware timer",
+}
+
+DATA["CONFIG_TIMER_10HZ_IRQ"] = {
+	"name": "Enable 10Hz timer interrupts",
+	"default": False,
+	"help": "Enables 10Hz interrupts on the hardware timer",
+}
+
 DATA["DEBUG"] = {
 	"name": "Debug",
 	"default": False,

drivers/rtca.c
@@ -50,6 +50,7 @@ static struct {
 
 void rtca_init(void)
 {
+#ifdef CONFIG_RTC_IRQ
 	/* Enable calendar mode (date/time registers are automatically reset)
 	and enable read ready interrupts
 	and set time event interrupts at each minute
@@ -61,6 +62,7 @@ void rtca_init(void)
 
 	/* Enable minutes interrupts */
 	RTCCTL01 |= RTCTEVIE;
+#endif
 }
 
 

drivers/timer.c
@@ -75,16 +75,20 @@ static uint16_t timer0_prog_ticks;
 /* this function setups a 1Hz timer ticked every overflow interrupt */
 void timer0_init(void)
 {
+#ifdef CONFIG_TIMER_1HZ_IRQ
 	/* Enable overflow interrupts */
 	TA0CTL |= TAIE;
+#endif
 
 	/* select external 32kHz source, /2 divider, continous mode */
 	TA0CTL |= TASSEL__ACLK | ID__2 | MC__CONTINOUS;
 
+#ifdef CONFIG_TIMER_1HZ_IRQ
 	/* setup and enable 100ms (10Hz) timer */
 	timer0_10hz_ticks = TIMER0_TICKS_FROM_MS(100);
 	TA0CCR0 = TA0R + timer0_10hz_ticks;
 	TA0CCTL0 |= CCIE;
+#endif
 }
 
 /* This function was based on original Texas Instruments implementation,

commit 08607866e7b23e1eb78099a0551b936b21fb7086

Fix typo introduced in f9d41e2aa197964334248c16ffb7538529afd262

tools/config.py
@@ -75,7 +75,7 @@ DATA["USE_WATCHDOG"] = {
 	"help": "Protects the clock against deadlocks by rebooting it.",
 }
 
-DATA["CONFIG_RTCA_IRQ"] = {
+DATA["CONFIG_RTC_IRQ"] = {
 	"name": "Enable RTC interrupts",
 	"default": True,
 	"help": "Enables interrupts on the Real Time Clock",