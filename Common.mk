e0cb6241cf1945af1fef45dcef4355e167080392
Angelo Arrifano <miknix@gmail.com>
2012-05-22 21:22:10 +0200

    Revert "Tell LD about the correct entry point"
    This reverts commit cf4ee2d61935d092d7c558b34f9ace891540d155.

--- Common.mk
+++ Common.mk
@@ -7,14 +7,12 @@ CC_DMACH	= -D__MSP430_6137__ -DMRFI_CC430 -D__CC430F6137__
 # -fdata-sections, -ffunction-sections and -Wl,--gc-sections -Wl,-s
 # are used for dead code elimination, see:
 # http://gcc.gnu.org/ml/gcc-help/2003-08/msg00128.html
-# "-Wl,--entry=main" is needed to tell LD that the entry point is the main
-# function. Looks like the default is some other symbol..
 #
 CFLAGS		+= $(CC_CMACH) $(CC_DMACH) -Os -Wall -fomit-frame-pointer
 CFLAGS		+= -fno-force-addr -finline-limit=1 -fno-schedule-insns
 CFLAGS		+= -fshort-enums -Wl,-Map=output.map
 CFLAGS		+= -fdata-sections -ffunction-sections
-LDFLAGS		= -Wl,--gc-sections -Wl,-s -Wl,--entry=main
+LDFLAGS		= -Wl,--gc-sections -Wl,-s
 INCLUDES	+= -I./ -Igcc/
 ### Build tools
 # 
