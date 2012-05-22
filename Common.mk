095b4ee6a60301692464d972dbc36731f2d26ea2
Angelo Arrifano <miknix@gmail.com>
2012-05-22 21:22:28 +0200

    Revert "Add dead code elimination flags to CFLAGS"
    This reverts commit 240d2df0ca3e83523ed83375dca8ba8c7d859018.

--- Common.mk
+++ Common.mk
@@ -4,15 +4,10 @@ CC_CMACH	= -mmcu=cc430f6137
 CC_DMACH	= -D__MSP430_6137__ -DMRFI_CC430 -D__CC430F6137__
 ### Build flags
 #
-# -fdata-sections, -ffunction-sections and -Wl,--gc-sections -Wl,-s
-# are used for dead code elimination, see:
-# http://gcc.gnu.org/ml/gcc-help/2003-08/msg00128.html
-#
 CFLAGS		+= $(CC_CMACH) $(CC_DMACH) -Os -Wall -fomit-frame-pointer
 CFLAGS		+= -fno-force-addr -finline-limit=1 -fno-schedule-insns
-CFLAGS		+= -fshort-enums -Wl,-Map=output.map
-CFLAGS		+= -fdata-sections -ffunction-sections
-LDFLAGS		= -Wl,--gc-sections -Wl,-s
+CFLAGS		+= -fshort-enums -ffunction-sections -Wl,-Map=output.map
+LDFLAGS		=
 INCLUDES	+= -I./ -Igcc/
 ### Build tools
 # 
