49dabe719729a63d3642a6e0a5d457c593260a01
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2012-06-20 16:51:16 +0200

    Added python script to create ringtones
    The support is very crude, and it should probably be merged in the
    Chronos tool, but here it is.
    
    Usage: rtttl2bin [string]
    
    Outputs the binary ringtone directly in C format, so it can be
    copy-pasted directly into OpenChronos-ng source code. It dies with an
    error if a note is too long to be stored, I must still think about what
    to do in this case (the longest note representable is 1023ms long).

---
+++ contrib/rtttl2bin.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3.2
+import re
+
+notes_translate = ('p', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#')
+
+
+def parse_note(note, default_duration, default_octave):
+    """
+        This parses a note into a tuple of (duration, pitch, octave)
+    """
+    note = re.match('(^[0-9]{0,2})([a-gp]#?)([4-9]?)(\.?)', note.strip(), flags=re.IGNORECASE).groups()
+    duration = note[0] == '' and default_duration or int(note[0])
+    duration *= (note[3] == '' and [1] or [1.5])[0]
+    return (duration, note[1].lower(), note[2] == '' and default_octave or int(note[2]))
+
+
+def parse_ringtone(ringtone):
+    """
+        This method parses a RTTTL ringtone (as a string) to a dictionary in this form:
+        title: the title of the melody
+        melody: a list of tuple notes, as parsed by parse_note
+        whole: The duration in ms of the "whole" note
+    """
+    match = re.match('([a-zA-Z]+):\s*d=([0-9]{1,3}),o=([0-9]{1,3}),b=([0-9]{1,3}):(.*)', ringtone)
+    default_duration = int(match.group(2))
+    default_octave = int(match.group(3))
+    bpm = int(match.group(4))
+    whole = (60 * 1000 / bpm) * 4
+    notes = [parse_note(note, default_duration, default_octave) for note in match.group(5).split(',')]
+    return {"title": match.group(1), "melody": notes, "whole": whole}
+
+
+def generate_binary_ringtone(ringtone):
+    """
+        This method converts a ringtone as parsed by parse_ringtone in the binary representation,
+        converting each note using generate_binary_note()
+    """
+    melody_real = list()
+    melody = ringtone["melody"]
+    for note in melody:
+        melody_real.append(hex(generate_binary_note(note, ringtone["whole"])))
+    melody_real.append("0x000F")
+    return "note %s[%d] = {%s};" % (ringtone["title"], len(ringtone["melody"]) + 1, ', '.join(melody_real))
+
+
+def generate_binary_note(note, whole_note):
+    """
+        This method converts a tuple note as parsed by parse_note to the binary representation.
+    """
+    duration = whole_note / note[0]
+    if duration > 1023:
+        raise Exception("note duration too long")
+    tone = notes_translate.index(note[1])
+    octave = note[2] - 4
+    return (duration << 6) | (octave << 2) | tone
+    
+    
+if __name__ == '__main__':
+    import sys
+    print(generate_binary_ringtone(parse_ringtone(sys.argv[1])))
