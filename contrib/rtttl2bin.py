db9df3e3097db6ec315c197e531bceda289d8922
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2012-06-23 13:00:32 +0200

    Added copyright notice to both files
    Added shabang line to rtttl2bin_test.

--- contrib/rtttl2bin.py
+++ contrib/rtttl2bin.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3.2
+#
+# Copyright (C) 2012 Aljaž Srebrnič <a2piratesoft@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 
 notes_translate = ('p', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#')
