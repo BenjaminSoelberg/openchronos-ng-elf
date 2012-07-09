39c54f01ab603090cd911ddc92255ba9f4ebc151
Thomas Post <tpost@post-net.ch>
2012-07-09 22:37:39 +0200

    - Updated tide watch to work with new API's - Added edit mode to tide watch (a user can now enter the time of the next low tide)

--- modules/tide.c
+++ modules/tide.c
@@ -44,15 +44,15 @@ const uint16_t fullTideTime = (uint16_t)754;
 const uint16_t halfTideTime = (uint16_t)372;//.5;
 
 //state of the display
-TideDisplayState displayState = TideDisplayState_Graph;
-static struct lcd_screen screen[3];
-static struct lcd_screen *currentScreen;
+uint8_t moduleActivated = 0;
+uint8_t editModeActivated = 0;
+TideDisplayState activeDisplay = TideDisplayState_Graph;
 
-uint8_t* graphs[4] = {
-  (uint8_t*)"_[^]_",
-  (uint8_t*)"[^]_[",
-  (uint8_t*)"^]_[^",
-  (uint8_t*)"]_[^]"
+const char* graphs[4] = {
+  "_[^]_",
+  "[^]_[",
+  "^]_[^",
+  "]_[^]"
 };
 uint8_t graphOffset = 0;
 
@@ -94,10 +94,29 @@ struct Tide timeFromMinutes(uint16_t minutes) {
 
 // MARK: Drawing
 
+void blinkCol(uint8_t screen, uint8_t line) 
+{
+  switch(line) {
+    case 1:
+      display_symbol(screen, LCD_SEG_L1_COL, SEG_ON);
+      display_symbol(screen, LCD_SEG_L1_COL, BLINK_ON);
+      break;
+    case 2:
+      display_symbol(screen, LCD_SEG_L2_COL0, SEG_ON);
+      display_symbol(screen, LCD_SEG_L2_COL0, BLINK_ON);
+      break;
+    default:
+      break;
+  }
+}
+
 void drawScreen() 
 {
-  display_clear(NULL, 1);
-	display_clear(NULL, 2);
+  if(!moduleActivated || editModeActivated) return; //do nothing if not visible
+  
+  display_clear(0, 0);
+  display_clear(1, 0);
+  display_clear(2, 0);
 	
   uint16_t nowInMinutes = timeNowInMinutes();
   uint16_t leftUntilLow = timeInMinutes(tide);
@@ -114,50 +133,50 @@ void drawScreen()
   //line1 time
   if(leftUntilHigh < leftUntilLow) {
     //show time till high
-    display_chars(&screen[0], LCD_SEG_L1_3_2, _itoa(highTide.hoursLeft, 2, 1), SEG_SET);
-    display_chars(&screen[0], LCD_SEG_L1_1_0, _itoa(highTide.minutesLeft, 2, 0), SEG_SET);
-    display_symbol(&screen[0], LCD_SYMB_MAX, SEG_SET);
+    display_chars(0, LCD_SEG_L1_3_2, _sprintf("%02u", highTide.hoursLeft), SEG_SET);
+    display_chars(0, LCD_SEG_L1_1_0, _sprintf("%02u", highTide.minutesLeft), SEG_SET);
+    display_symbol(0, LCD_SYMB_MAX, SEG_ON);
     
   } else {
     //show time till low
-    display_chars(&screen[0], LCD_SEG_L1_3_2, _itoa(lowTide.hoursLeft, 2, 1), SEG_SET);
-    display_chars(&screen[0], LCD_SEG_L1_1_0, _itoa(lowTide.minutesLeft, 2, 0), SEG_SET);
-    display_symbol(&screen[0], LCD_UNIT_L2_MI, SEG_SET);
+    display_chars(0, LCD_SEG_L1_3_2, _sprintf("%02u", lowTide.hoursLeft), SEG_SET);
+    display_chars(0, LCD_SEG_L1_1_0, _sprintf("%02u", lowTide.minutesLeft), SEG_SET);
+    display_symbol(0, LCD_UNIT_L2_MI, SEG_ON);
   }
-  display_symbol(&screen[0], LCD_SEG_L1_COL, SEG_SET);
-  display_symbol(&screen[0], LCD_SEG_L1_COL, BLINK_ON);
+  blinkCol(0,1);
+  display_symbol(0, LCD_SEG_L2_COL1, SEG_ON);
   
   //line 2 graph
-  display_chars(&screen[0], LCD_SEG_L2_4_0, (uint8_t*)graphs[graphOffset], SEG_SET);
-  display_symbol(&screen[0], LCD_SEG_L2_COL1, SEG_ON);
-  
+  display_chars(0, LCD_SEG_L2_4_0, graphs[graphOffset], SEG_SET);  
   
   //screen 1
   //line 1 time till low
-  display_chars(&screen[1], LCD_SEG_L1_3_2, _itoa(lowTide.hoursLeft, 2, 1), SEG_SET);
-  display_chars(&screen[1], LCD_SEG_L1_1_0, _itoa(lowTide.minutesLeft, 2, 0), SEG_SET);
-  display_symbol(&screen[1], LCD_UNIT_L2_MI, SEG_SET);
+  display_chars(1, LCD_SEG_L1_3_2, _sprintf("%02u", lowTide.hoursLeft), SEG_SET);
+  display_chars(1, LCD_SEG_L1_1_0, _sprintf("%02u", lowTide.minutesLeft), SEG_SET);
+  display_symbol(1, LCD_UNIT_L2_MI, SEG_ON);
   
   //line 2 calculate time of next high
   uint16_t lowTideTimeInMinutes = (nowInMinutes + leftUntilLow) % twentyFourHoursInMinutes;
   struct Tide lowTideTime = timeFromMinutes(lowTideTimeInMinutes);
-  display_chars(&screen[1], LCD_SEG_L2_3_2, _itoa(lowTideTime.hoursLeft, 2, 1), SEG_SET);
-  display_chars(&screen[1], LCD_SEG_L2_1_0, _itoa(lowTideTime.minutesLeft, 2, 0), SEG_SET);
+  display_chars(1, LCD_SEG_L2_3_2, _sprintf("%02u", lowTideTime.hoursLeft), SEG_SET);
+  display_chars(1, LCD_SEG_L2_1_0, _sprintf("%02u", lowTideTime.minutesLeft), SEG_SET);
+  blinkCol(1,1);
+  blinkCol(1,2);
     
     
   // screen 2  
   // Line 1 time high
-  display_chars(&screen[2], LCD_SEG_L1_3_2, _itoa(highTide.hoursLeft, 2, 1), SEG_SET);
-  display_chars(&screen[2], LCD_SEG_L1_1_0, _itoa(highTide.minutesLeft, 2, 0), SEG_SET);
-  display_symbol(&screen[2], LCD_SYMB_MAX, SEG_SET);
+  display_chars(2, LCD_SEG_L1_3_2, _sprintf("%02u", highTide.hoursLeft), SEG_SET);
+  display_chars(2, LCD_SEG_L1_1_0, _sprintf("%02u", highTide.minutesLeft), SEG_SET);
+  display_symbol(2, LCD_SYMB_MAX, SEG_ON);
   
   //line 2 calculate time of next high
   uint16_t highTideTimeInMinutes = (nowInMinutes + leftUntilHigh) % twentyFourHoursInMinutes;
   struct Tide highTideTime = timeFromMinutes(highTideTimeInMinutes);
-  display_chars(&screen[2], LCD_SEG_L2_3_2, _itoa(highTideTime.hoursLeft, 2, 1), SEG_SET);
-  display_chars(&screen[2], LCD_SEG_L2_1_0, _itoa(highTideTime.minutesLeft, 2, 0), SEG_SET);
-  
-  lcd_screen_virtual_to_real(currentScreen);
+  display_chars(2, LCD_SEG_L2_3_2, _sprintf("%02u", highTideTime.hoursLeft), SEG_SET);
+  display_chars(2, LCD_SEG_L2_1_0, _sprintf("%02u", highTideTime.minutesLeft), SEG_SET);
+  blinkCol(2,1);
+  blinkCol(2,2);
 }
 
 // MARK: System Bus Events
@@ -184,116 +203,120 @@ void minuteTick()
 }
 
 // MARK: - Time Setup
-
-void displayEditValue()
+void editHHSelect()
 {
-	display_clear(NULL, 1);
-	display_clear(NULL, 2);
-	
-  switch(fieldBeeingEdited) {
-    case 0:
-      display_chars(NULL, LCD_SEG_L1_3_2, _itoa(enteredTimeOfNextLow.hoursLeft, 2, 0), SEG_ON);
-      display_chars(NULL, LCD_SEG_L1_1_0, _itoa(enteredTimeOfNextLow.minutesLeft, 2, 0), SEG_ON);
-      display_chars(NULL, LCD_SEG_L1_3_2, NULL, BLINK_ON);
-      display_chars(NULL, LCD_SEG_L1_1_0, NULL, BLINK_OFF);
-      break;
-    case 1:
-      display_chars(NULL, LCD_SEG_L1_3_2, _itoa(enteredTimeOfNextLow.hoursLeft, 2, 0), SEG_ON);
-      display_chars(NULL, LCD_SEG_L1_1_0, _itoa(enteredTimeOfNextLow.minutesLeft, 2, 0), SEG_ON);
-      display_chars(NULL, LCD_SEG_L1_3_2, NULL, BLINK_OFF);
-      display_chars(NULL, LCD_SEG_L1_1_0, NULL, BLINK_ON);
-      break;
-    default:
-      break;
-  }
+  display_chars(0, LCD_SEG_L1_3_2, NULL, BLINK_ON);
 }
 
-void changeValue(int8_t amount)
+void editHHDeselect()
 {
-  switch(fieldBeeingEdited) {
-    case 0:
-      enteredTimeOfNextLow.hoursLeft = (enteredTimeOfNextLow.hoursLeft + amount) % 24;
-      break;
-    case 1:
-      enteredTimeOfNextLow.minutesLeft = (enteredTimeOfNextLow.minutesLeft + amount) % 60; 
-      break;
-    default:
-      break;
-  }
-  displayEditValue();
+  display_chars(0, LCD_SEG_L1_3_2, NULL, BLINK_OFF);
 }
 
-void startEditingNextValue()
+void editHHSet(int8_t step) 
 {
-  fieldBeeingEdited = (fieldBeeingEdited + 1) % 2;
-  displayEditValue();
+  helpers_loop(&(enteredTimeOfNextLow.hoursLeft), 0, 23, step);
+  display_chars(0, LCD_SEG_L1_3_2, _sprintf("%02u", enteredTimeOfNextLow.hoursLeft), SEG_SET);
 }
+
+void editMMSelect()
+{
+  display_chars(0, LCD_SEG_L1_1_0, NULL, BLINK_ON);
+}
+
+void editMMDeselect()
+{
+  display_chars(0, LCD_SEG_L1_1_0, NULL, BLINK_OFF);
+}
+
+void editMMSet(int8_t step) 
+{
+  helpers_loop(&(enteredTimeOfNextLow.minutesLeft), 0, 59, step);
+  display_chars(0, LCD_SEG_L1_1_0, _sprintf("%02u", enteredTimeOfNextLow.minutesLeft), SEG_SET);
+}
+
+static struct menu_editmode_item editModeItems[] = {
+	{&editHHSelect, &editHHDeselect, &editHHSet},
+	{&editMMSelect, &editMMDeselect, &editMMSet},
+	{ NULL }
+};
 
 void endEditing()
 {
-  tide = enteredTimeOfNextLow;
-	display_clear(NULL, 1);
-	display_clear(NULL, 2);
+  //calculate minutes left for next low
+  uint16_t nextLowMinutes = timeInMinutes(enteredTimeOfNextLow);
+  uint16_t nowInMinutes = timeNowInMinutes();
+  uint16_t diff = nextLowMinutes - nowInMinutes;
+  if(diff < 0) {
+    //the next low is tomorrow!
+    diff = (twentyFourHoursInMinutes - nowInMinutes) + nextLowMinutes;
+  }
+  
+  tide = timeFromMinutes(diff);
+  editModeActivated = 0;
+	display_clear(0, 0);
 	drawScreen();
 }
 
 // MARK:  - Buttons
-void longNumberButton() 
+void longStarButton() 
 {
   //clear screen
-  display_clear(NULL, 1);
-	display_clear(NULL, 2);
+  display_clear(0, 0);
+	lcd_screen_activate(0);
 	
-	enteredTimeOfNextLow = tide;
-  menu_editmode_start(&changeValue, &startEditingNextValue, &endEditing);
+  uint16_t nowInMinutes = timeNowInMinutes();
+  uint16_t leftUntilLow = timeInMinutes(tide);
+  enteredTimeOfNextLow = timeFromMinutes((nowInMinutes + leftUntilLow) % twentyFourHoursInMinutes);
+  
+	editModeActivated = 1;
+  display_chars(0, LCD_SEG_L1_3_2, _sprintf("%02u", enteredTimeOfNextLow.hoursLeft), SEG_SET);
+  display_chars(0, LCD_SEG_L1_1_0, _sprintf("%02u", enteredTimeOfNextLow.minutesLeft), SEG_SET);  
+  blinkCol(0, 1);
+  menu_editmode_start(&endEditing, editModeItems);
 }
 
 void buttonUp()
 {
-  displayState = (displayState + 1 ) % 3;
-  currentScreen = &screen[displayState];
+  lcd_screen_activate(0xff);
   drawScreen();
 }
 
 void buttonDown() 
 {
-  if(displayState <= 0) {
-    displayState = 2;
+  if(activeDisplay <= 0) {
+    activeDisplay = 2;
   } else {
-    displayState = (displayState - 1 ) % 3;
+    activeDisplay = (activeDisplay - 1 ) % 3;
   }
-  currentScreen = &screen[displayState];
+  lcd_screen_activate(activeDisplay);
   drawScreen();
 }
 
 // MARK: - Activate and Deactivate
 void activate()
 {
+  moduleActivated = 1;
   	/* create three empty screens */
-	lcd_screen_create(&screen[0]);
-	lcd_screen_create(&screen[1]);
-	lcd_screen_create(&screen[2]);
+	lcd_screens_create(3);
 	
-	displayState = TideDisplayState_Graph;
-	currentScreen = &screen[displayState];
+	activeDisplay = TideDisplayState_Graph;
+	lcd_screen_activate(activeDisplay);
 	drawScreen();
 }
 
 void deactivate()
 {
+  moduleActivated = 0;
   /* destroy virtual screens */
-	lcd_screen_destroy(&screen[0]);
-	lcd_screen_destroy(&screen[1]);
-	lcd_screen_destroy(&screen[2]);
+	lcd_screens_destroy();
 		
-	display_clear(NULL, 1);
-	display_clear(NULL, 2);
-}
+	display_clear(0, 0);}
 
 void tide_init()
 {   
   sys_messagebus_register(&minuteTick, SYS_MSG_RTC_MINUTE);
-  menu_add_entry(&buttonUp, &buttonDown, NULL, NULL, &longNumberButton, NULL, &activate, &deactivate);
+  menu_add_entry("TIDE", &buttonUp, &buttonDown, NULL, &longStarButton, NULL, NULL, &activate, &deactivate);
   tide = timeFromMinutes(90);//fullTideTime);
   minuteTick(); //initla display setup
 }
