6579bc2e6330ef60673c788247db979ab91e7e15
Angelo Arrifano <miknix@gmail.com>
2012-11-17 15:40:28 +0100

    Cleanup temperature driver
    Code formatting fix and other small non-cosmetic fixes.

--- drivers/temperature.h
+++ drivers/temperature.h
@@ -16,82 +16,31 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-// *************************************************************************************************
-//
-//	Copyright (C) 2009 Texas Instruments Incorporated - http://www.ti.com/
-//
-//
-//	  Redistribution and use in source and binary forms, with or without
-//	  modification, are permitted provided that the following conditions
-//	  are met:
-//
-//	    Redistributions of source code must retain the above copyright
-//	    notice, this list of conditions and the following disclaimer.
-//
-//	    Redistributions in binary form must reproduce the above copyright
-//	    notice, this list of conditions and the following disclaimer in the
-//	    documentation and/or other materials provided with the
-//	    distribution.
-//
-//	    Neither the name of Texas Instruments Incorporated nor the names of
-//	    its contributors may be used to endorse or promote products derived
-//	    from this software without specific prior written permission.
-//
-//	  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//	  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-//	  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-//	  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-//	  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-//	  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//	  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-//	  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-//	  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-//	  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-//	  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// *************************************************************************************************
 
-#ifndef TEMPERATURE_H_
-#define TEMPERATURE_H_
+#ifndef __TEMPERATURE_H__
+#define __TEMPERATURE_H__
 
-// *************************************************************************************************
-// Include section
 #include <openchronos.h>
 
-// *************************************************************************************************
-// Prototypes section
-
-// internal functions
-extern void temperature_init(void);
-extern void temperature_measurement(uint8_t filter);
+void temperature_init(void);
+void temperature_measurement(uint8_t filter);
 
 #ifndef CONFIG_TEMPERATURE_METRIC_ONLY
-extern int16_t convert_C_to_F(int16_t value);
-extern int16_t convert_F_to_C(int16_t value);
+int16_t convert_C_to_F(int16_t value);
+int16_t convert_F_to_C(int16_t value);
 #endif
-// *************************************************************************************************
-// Defines section
 
-
-// *************************************************************************************************
-// Global Variable section
 /* TODO: pack this stuff!!!! */
-struct temp
-{
-	// Temperature (�C) in 2.1 format
+struct temp {
+	/* Temperature (oC) in 2.1 format */
 	int16_t		degrees;
-	// User set calibration value (�C) in 2.1 format
+	/* User set calibration value (oC) in 2.1 format */
 	int16_t		offset;
 #ifndef CONFIG_TEMPERATURE_METRIC_ONLY
-    //C or F
-    uint8_t         is_c :1;
+	/* C or F */
+	uint8_t         is_c:1;
 #endif
 
-};
-extern struct temp sTemp;
+} sTemp;
 
-// *************************************************************************************************
-// Extern section
-
-
-#endif /*TEMPERATURE_H_*/
+#endif /* __TEMPERATURE_H__ */
