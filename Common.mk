e73ee6d44b0ba588a980f15139e4cc868469a4ea
Benjamin Sølberg <benjamin.soelber@gmail.com>
2016-01-08 20:03:32 +0100

    Updated to use the gcc compiler MSP430-elf from TI

--- Common.mk
+++ Common.mk
@@ -11,7 +11,7 @@ CC_DMACH	= -D__MSP430_6137__ -DMRFI_CC430 -D__CC430F6137__
 CFLAGS		+= $(CC_CMACH) $(CC_DMACH) -Wall
 CFLAGS		+= -fno-force-addr -finline-limit=1 -fno-schedule-insns
 CFLAGS		+= -fshort-enums -Wl,-Map=output.map
-LDFLAGS		=
+LDFLAGS		= -L$(MSP430_TI)/include
 
 CFLAGS_REL	+= -Os -fdata-sections -ffunction-sections -fomit-frame-pointer
 LDFLAGS_REL	+= -Wl,--gc-sections -Wl,-s
@@ -20,10 +20,10 @@ CFLAGS_DBG	+= -O1 -ggdb
 LDFLAGS_DBG	+= -Wl,--gc-sections
 
 # linker flags and include directories
-INCLUDES	+= -I./ -Igcc/
+INCLUDES	+= -I./ -I$(MSP430_TI)/include
 ### Build tools
 # 
-CC		= msp430-gcc
-LD		= msp430-ld
-AS		= msp430-as
-AR		= msp430-ar
+CC		= msp430-elf-gcc
+LD		= msp430-elf-ld
+AS		= msp430-elf-as
+AR		= msp430-elf-ar
