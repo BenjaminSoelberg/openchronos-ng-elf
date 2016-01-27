031a19060e86ffda9a0e7325ffd4372ec9a147f4
Benjamin Sølberg <benjamin.soelberg@gmail.com>
2016-01-27 22:38:55 +0100

    circleci:updated setup shell script for circle ci build server

--- circleci/setup.sh
+++ circleci/setup.sh
@@ -1,12 +1,22 @@
 rm -rf ~/cached
 
 mkdir -p ~/cached
-mkdir -p ~/temp/msp430-gcc-source/msp430-gcc-obj
-cd ~/temp/msp430-gcc-source
-wget https://github.com/BenjaminSoelberg/msp430-elf/archive/sources.zip -O msp430-elf.zip
-unzip msp430-elf.zip >/dev/null
+
+mkdir -p ~/temp
+cd ~/temp
+wget https://github.com/BenjaminSoelberg/msp430-elf/archive/sources.zip -O msp430-elf-source.zip
+unzip msp430-elf-source.zip > unzip.out
+tail unzip.out
+
+cd ~/temp/msp430-elf-source
+mkdir -p msp430-gcc-obj
 cd  msp430-gcc-obj
 ../tools/configure --prefix=~/msp430-elf-gcc --target=msp430-elf --enable-languages=c,c++ --disable-itcl --disable-tk --disable-tcl --disable-libgui --disable-gdbtk
-make
-#make install
+
+make >make.out
+tail make.out
+
+make install>install.out
+tail install.txt
+
 rm -rf ~/cached
