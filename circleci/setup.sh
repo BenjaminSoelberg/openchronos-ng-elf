4e91fd7d668108aa5d60b85682432d2589b969c5
Benjamin Sølberg <benjamin.soelberg@gmail.com>
2016-01-28 00:01:58 +0100

    circleci:updated setup shell script for circle ci build server

--- circleci/setup.sh
+++ circleci/setup.sh
@@ -1,21 +1,26 @@
+
+if [ ! -d ~/cache/msp430-elf-gcc ]; then exit; fi
+#!/bin/bash
 mkdir -p ~/cached
 
+if [ ! -d ~/cached/msp430-elf-gcc ]; then exit 0; fi
+
+echo Downloading msp-430-elf-gcc...
 mkdir -p ~/temp
 cd ~/temp
 wget https://github.com/BenjaminSoelberg/msp430-elf/archive/sources.zip -O msp430-elf-sources.zip
-unzip msp430-elf-sources.zip >unzip.out 2>&1
-tail unzip.out
+unzip msp430-elf-sources.zip
 
+echo Configuring msp-430-elf-gcc...
 cd ~/temp/msp430-elf-sources
 mkdir -p msp430-gcc-obj
 cd  msp430-gcc-obj
-../tools/configure --prefix=/home/ubuntu/cached/msp430-elf-gcc --target=msp430-elf --enable-languages=c,c++ --disable-itcl --disable-tk --disable-tcl --disable-libgui --disable-gdbtk >../configure.txt 2>&1
-tail ../configure.txt
+../tools/configure --prefix=/home/ubuntu/cached/msp430-elf-gcc --target=msp430-elf --enable-languages=c,c++ --disable-itcl --disable-tk --disable-tcl --disable-libgui --disable-gdbtk
 
-make >../make.out 2>&1
-tail ../make.out
+echo Compiling msp-430-elf-gcc...
+make
 
-make install >../install.out 2>&1
-tail ../install.txt
+echo Installing msp-430-elf-gcc...
+make install
 
 #rm -rf ~/cached
