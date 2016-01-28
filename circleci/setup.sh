94b931b2385402a76412ec91087c097d9143562b
Benjamin Sølberg <benjamin.soelberg@gmail.com>
2016-01-28 11:39:17 +0100

    Update setup.sh

--- circleci/setup.sh
+++ circleci/setup.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-if [ -d ~/cached/msp430-elf-gcc ]; then exit 0; fi
+#Setup script to download and build the TI open source MSP430 elf GCC compiler
+set CACHE=$HOME/cached
+set GCC_HOME=$CACHE/msp430-elf-gcc
 
 mkdir -p ~/cached
-
-echo Downloading msp-430-elf-gcc...
 mkdir -p ~/temp
 cd ~/temp
-wget https://github.com/BenjaminSoelberg/msp430-elf/archive/sources.zip -O msp430-elf-sources.zip
-unzip msp430-elf-sources.zip
 
-echo Configuring msp-430-elf-gcc...
-cd ~/temp/msp430-elf-sources
-mkdir -p msp430-gcc-obj
-cd  msp430-gcc-obj
-../tools/configure --prefix=$HOME/cached/msp430-elf-gcc --target=msp430-elf --enable-languages=c,c++ --disable-itcl --disable-tk --disable-tcl --disable-libgui --disable-gdbtk
+if [ ! -d $GCC_HOME/include ]; then
+  echo Downloading msp430 includes...
+  wget https://github.com/BenjaminSoelberg/msp430-elf/archive/gcc_rh.zip -O msp430-elf-gcc_rh.zip
+  unzip msp430-elf-gcc_rh.zip
+  cp -r msp430-elf-gcc_rh/* $GCC_HOME
+fi
 
-echo Compiling msp-430-elf-gcc...
-make
+if [! -d $GCC_HOME ]; then
+  echo Downloading msp430 elf gcc compiler source...
 
-echo Installing msp-430-elf-gcc...
-make install
+  wget https://github.com/BenjaminSoelberg/msp430-elf/archive/sources.zip -O msp430-elf-sources.zip
+  unzip msp430-elf-sources.zip
 
-#rm -rf ~/cached
+  echo Configuring msp-430-elf-gcc...
+  cd msp430-elf-sources
+  mkdir -p msp430-gcc-obj
+  cd  msp430-gcc-obj
+  ../tools/configure --prefix=$HOME/cached/msp430-elf-gcc --target=msp430-elf --enable-languages=c,c++ --disable-itcl --disable-tk --disable-tcl --disable-libgui --disable-gdbtk
+
+  echo Compiling msp-430-elf-gcc...
+  make
+
+  echo Installing msp-430-elf-gcc...
+  make install
+fi
