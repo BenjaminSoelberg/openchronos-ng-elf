d5c0a9988d8f8590eaf61242633118577ef1e2d7
Benjamin Sølberg <benjamin.soelberg@gmail.com>
2016-12-30 17:57:22 +0100

    Extracted sha1 code from otp module into own files to the best of my abilities

--- modules/sha1.h
+++ modules/sha1.h
@@ -15,12 +15,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef  __OTP_H__
-#define  __OTP_H__
+#ifndef SHA1_H__
+#define SHA1_H__
 
 #include <stdint.h>
 
-void hmac_sha1(const uint8_t *key, int keyLength,
-               const uint8_t *data, int dataLength,
-               uint8_t *result, int resultLength);
+#define SHA1_BLOCKSIZE     64
+#define SHA1_DIGEST_LENGTH 20
+
+typedef struct {
+    uint32_t digest[8];
+    uint32_t count_lo, count_hi;
+    uint8_t  data[SHA1_BLOCKSIZE];
+    int      local;
+} SHA1_INFO;
+
+void sha1_init(SHA1_INFO *sha1_info) __attribute__((visibility("hidden")));
+void sha1_update(SHA1_INFO *sha1_info, const uint8_t *buffer, int count)
+__attribute__((visibility("hidden")));
+void sha1_final(SHA1_INFO *sha1_info, uint8_t digest[20])
+__attribute__((visibility("hidden")));
+
 #endif
